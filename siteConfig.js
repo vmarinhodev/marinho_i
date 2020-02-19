42b278cf33f846a39cc187e8a9dafab0083af7cd
vmarinhodev <vitor@vmarinho.dev>
2020-02-19 22:25:03 +0100

    Added siteConfig file

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'vmarinho',
+  shortname: 'vm.dev',
+  description: 'A portfolio website Gatsby theme for V Marinho',
+  url: 'https://vmarinho.dev',
+  prefix: '/',
+  author: 'Vitor Marinho',
+  twitter: ''
+}
